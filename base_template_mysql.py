b8b599c8d3a45b8c9a7aad7dc3fd14b5d755eec6
Tyler <53105203+TheCodingMarine@users.noreply.github.com>
2022-03-14 18:41:28 -0400

    2

--- base_template_mysql.py
+++ base_template_mysql.py
@@ -1,12 +1,24 @@
 import os
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import (MetaData, create_engine, Table, Column, Integer,
+                        Numeric, String, ForeignKey, DateTime, null, 
+                        ForeignKey, Boolean)
+from datetime import datetime
+
 
 metadata = MetaData()
-
-
 username = os.environ.get('USERNAME')
 password = os.environ.get('PASSWORD')
 
-engine = create_engine('mysql+pymysql://' + username + ':' + password + \
+#connect to the server and create a database 
+mysql_engine = create_engine('mysql+pymysql://' + username + ':' + password + \
                         '@localhost/')
-metadata.create_all(engine)
+connection = mysql_engine.connect()
+connection.execute("CREATE DATABASE <some_db_name>")
+connection.execute("USE <some_db_name>")
+
+# Create metadata here for tables.
+
+# Create engine to connect to our database and persist the tables.
+'<some_db_name>'_engine = create_engine('mysql+pymysql://' + username + ':' + password + \
+                        '@localhost/<some_db_name>')
+metadata.create_all('<some_db_name>'_engine)
